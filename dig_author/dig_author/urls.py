a88e18cfb3ed816ee06a375e4288c79b61f11e73
XFJH <XFJH@github.com>
2015-04-29 05:41:52 +0800

    add django_cms
    add django_cms

--- dig_author/dig_author/urls.py
+++ dig_author/dig_author/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.core.mail import send_mail
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,13 +14,21 @@ urlpatterns = patterns('',
     url(r'^weblog/', include('zinnia.urls', namespace='zinnia')),
     url(r'^comments/', include('django_comments.urls')),
     url(r'weixin/', include('weixin.urls', namespace='weixin')),
-    url(r'^', include('digs.urls', namespace='digs')),
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^', include('digs.urls', namespace='digs')), # dig entrance
+    # url(r'^admin/', include(admin.site.urls)),
 )
 
+urlpatterns += i18n_patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # customizing error views
+
 handler500 = 'digs.views.page_error'
 
 # Sending email
 send_mail('Subject here', 'Here is the message.', '1258080923@qq.com',
     ['941078420@qq.com'], fail_silently=False)
+
+handler500 = 'digs.views.page_error'
+
